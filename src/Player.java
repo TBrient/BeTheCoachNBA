758232f7f03ec0c581cac1b74aaf6bb5fd211dc9
jjremy <jeremy_kunen@student.wayland.k12.ma.us>
2017-12-20 07:42:25 -0500

    ye

--- src/Player.java
+++ src/Player.java
@@ -10,4 +10,28 @@ public class Player {
         this.rating = rating;
         this.age = age;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
--- src/Team.java
+++ src/Team.java
@@ -17,4 +17,20 @@ public class Team {
     public void setTeam(ArrayList<Player> team) {
         this.team = team;
     }
+
+    public int[] setRecord(){
+        int score = 0;
+        int ans[] = new int[2];
+        for (int i = 0; i < team.size(); i++) {
+            score += team.get(i).getRating();
+        }
+        ans[0] = 82 * ( 1 - (score / 40) * 2);
+        ans[1] = 82 - ans[0];
+        return ans;
+    }
+    public void simRecords(ArrayList<Team> teams){
+        for (int i = 0; i < teams.size(); i++) {
+            teams.get(i).setRecord();
+        }
+    }
 }
